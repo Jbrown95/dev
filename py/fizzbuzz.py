1f83f1a6fb733c0517839ba9d29efbe0afaf7111
Joshua Brown <joshuadbrown95@gmail.com>
2018-01-16 17:19:33 -0500

    created new git repo with random projects

---
+++ py/fizzbuzz.py
@@ -0,0 +1,12 @@
+
+number = raw_input("input a number for fizzbuzz:")
+number = int(number)
+for num in range(1,number):
+        if num % 15 == 0:
+            print(str(num) + ':fizzbuzz')
+        elif num % 3 == 0:
+            print(str(num) + ':fizz')
+        elif num % 5 == 0:
+            print(str(num) + ':buzz')
+        else:
+            print(str(num) + ":")
